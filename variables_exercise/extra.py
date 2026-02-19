7a77ff90c3e4a0660c4f437e2450878e55b0b088
Erica Motloutsi <ericamotloutsi@gmail.com>
2026-02-19 12:40:01 +0000

    message

--- variables_exercise/extra.py
+++ variables_exercise/extra.py
@@ -62,4 +62,59 @@ else:
     print("senior")
 
 
+def greet():
+    print("hey")
+    print("programmers")
+
+def whistle():
+    print("doot")
+
+print("first")
+print("second")
+greet()
+print("third")
+print("fourth")
+whistle()
+
+def how_many():
+    return 42
+
+print(how_many)
+print(how_many())
+
+the_answer = how_many()
+print(the_answer)
+
+def how_much():
+    5   # does nothing
+
+print(how_much())
+
+def average(num1, num2):
+    print("calculating...")
+    return (num1 + num2) / 2
+
+print(average(5, 10))
+print(average(20, 26))
+print(average(50, 100) + 2)
+
+a = 21 + 3
+b = 20
+n = average(a, b)
+print(average(n, 18))
+
+def exclaim(s):
+    capitalized = s.upper()
+    return capitalized + "!!"
+
+result = exclaim("potato")
+print(result)
+print(len(result))
+print(result[0])
+print(result[-1])
+
+
+
+
+
 
