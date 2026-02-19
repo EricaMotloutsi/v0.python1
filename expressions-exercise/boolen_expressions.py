50b2338e290038e9b830f5d64800d882bc63fef2
Erica Motloutsi <ericamotloutsi@gmail.com>
2026-02-19 10:56:59 +0000

    message

---
+++ expressions-exercise/boolen_expressions.py
@@ -0,0 +1,15 @@
+print(False)
+print(not True)
+print(not False)
+print(not not True)
+ 
+ print(False and False)
+print(False and True)
+print(True and False)
+print(True and True)
+
+print(False or False)
+print(False or True)
+print(True or False)
+print(True or True)
+
