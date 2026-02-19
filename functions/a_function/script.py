3877da19d8ff5f20bda27fd9e05ad2a1d113f4b0
Erica Motloutsi <ericamotloutsi@gmail.com>
2026-02-19 20:16:14 +0000

    add function

--- functions/a_function/script.py
+++ functions/a_function/script.py
@@ -22,4 +22,43 @@ print("third")
 print("fourth")
 whistle()
 
+def how_many():
+    return 42
+
+print(how_many)
+print(how_many())
+
+the_answer = how_many()
+print(the_answer)
+
+def how_much():
+    5   # does nothing
+
+print(how_much())
+
+def average(num1, num2):
+    print("calculating...")
+    return (num1 + num2) / 2
+
+print(average(5, 10))
+print(average(20, 26))
+print(average(50, 100) + 2)
+
+a = 21 + 3
+b = 20
+n = average(a, b)
+print(average(n, 18))
+
+def exclaim(s):
+    capitalized = s.upper()
+    return capitalized + "!!"
+
+result = exclaim("potato")
+print(result)
+print(len(result))
+print(result[0])
+print(result[-1])
+
+
+
 
