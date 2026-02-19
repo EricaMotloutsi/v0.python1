070a4fb2ce73d56c07960aeb42df7f5db0999ac7
Erica Motloutsi <ericamotloutsi@gmail.com>
2026-02-19 21:44:50 +0000

    message

--- functions/half.py
+++ functions/half.py
@@ -1,1 +1,12 @@
+# Write a function `half` that accepts a number as an argument.
+# The function should return half of the number.
 
+print(half(8))    # 4
+print(half(15))   # 7.5
+print(half(90))   # 45
+
+def half(num):
+    return num / 2
+print(half(8))
+print(half(15))
+print(half(90))
