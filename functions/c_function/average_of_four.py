75278e424d9a7d2008418156bf24f886a68fab94
Erica Motloutsi <ericamotloutsi@gmail.com>
2026-02-20 01:59:49 +0000

    add function

---
+++ functions/c_function/average_of_four.py
@@ -0,0 +1,18 @@
+#Write average_of_four(a, b, c, d) that returns the average of four numbers.
+print(average_of_four(10, 4, 12, 3))     # 7.25
+print(average_of_four(-20, 50, 4, 21))   # 13.75
+print(average_of_four(5, 5, 3, 7))       # 5
+
+def average_of_four(num1,num2,num3,num4):
+    return average_of_four (num1+num2+num3+num4)/4
+print(average_of_four(10, 4, 12, 3))     # 7.25
+print(average_of_four(-20, 50, 4, 21))   # 13.75
+print(average_of_four(5, 5, 3, 7))   
+
+def average_of_four(num1, num2, num3, num4):
+    return (num1 + num2 + num3 + num4) / 4
+
+
+print(average_of_four(10, 4, 12, 3))     # 7.25
+print(average_of_four(-20, 50, 4, 21))   # 13.75
+print(average_of_four(5, 5, 3, 7))       # 5.0
