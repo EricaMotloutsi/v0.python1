b9bb4ac4dfb059cdf778fd1ebf937c016107e2be
Erica Motloutsi <ericamotloutsi@gmail.com>
2026-02-20 02:20:34 +0000

    add function

---
+++ functions/c_function/reverse_string.py
@@ -0,0 +1,8 @@
+#Write a function reverse_string(text) that returns the reversed string.
+print(reverse_string("hello"))   # olleh
+print(reverse_string("Python"))  # nohtyP
+
+def reverse_string(string):
+    return string[::-1]
+print(reverse_string("hello"))   # olleh
+print(reverse_string("Python"))  
