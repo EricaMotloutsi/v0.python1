6c058bc6be1c79ea969be5b880cc3124104d245d
Erica Motloutsi <ericamotloutsi@gmail.com>
2026-02-20 02:30:11 +0000

    add function

--- functions/c_function/sum_upto.py
+++ functions/c_function/sum_upto.py
@@ -4,3 +4,12 @@
 print(sum_upto(5))   # 15
 print(sum_upto(10))  # 55
 
+def sum_upto(n):
+    total = 0
+    for i in range(1, n + 1):
+        total += i
+    return total
+
+
+print(sum_upto(5))   # 15
+print(sum_upto(10))  # 55
