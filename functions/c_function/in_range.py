95e7531d369b2a7831f362d959a1e579d08524c3
Erica Motloutsi <ericamotloutsi@gmail.com>
2026-02-20 02:09:34 +0000

    add function

--- functions/c_function/in_range.py
+++ functions/c_function/in_range.py
@@ -8,17 +8,3 @@ print(in_range(100, 125, 99)) # False
 print(in_range(40, 45, 44))   # True
 print(in_range(40, 45, 45))   # True
 print(in_range(40, 45, 46))   # False
-
-def in_range(min,max,num):
-    if num>=min and num<=max:
-        return True 
-    else:
-       return False
-
-print(in_range(5, 13, 8))     # True
-print(in_range(5, 13, 29))    # False
-print(in_range(100, 125, 100))# True
-print(in_range(100, 125, 99)) # False
-print(in_range(40, 45, 44))   # True
-print(in_range(40, 45, 45))   # True
-print(in_range(40, 45, 46))
