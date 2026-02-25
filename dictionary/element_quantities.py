22255fb5921a58a9234a700cf322b37997515a1a
Erica Motloutsi <ericamotloutsi@gmail.com>
2026-02-25 13:25:08 +0000

    Add dictionary: quantities

---
+++ dictionary/element_quantities.py
@@ -0,0 +1,19 @@
+
+def element_quantities(dictionary):
+    ans_list = []
+    for key, value in dictionary.items():
+        for i in range(value):
+            ans_list.append(key)
+    return ans_list
+
+
+
+
+quantities1 = {"cat":3,"bird":1,"dog":2 }
+print(element_quantities(quantities1))
+# ['cat', 'cat', 'cat', 'bird', 'dog', 'dog']
+
+quantities2 = {"blue":3,"brown":1 }
+print(element_quantities(quantities2))
+# ['blue', 'blue', 'blue', 'brown']
+
