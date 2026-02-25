93817482e68df12e8fae27d96917a2404cd820b1
Erica Motloutsi <ericamotloutsi@gmail.com>
2026-02-25 11:57:24 +0000

    Add remove dupes function

---
+++ nesting/c_nesting/remove_dupes.py
@@ -0,0 +1,16 @@
+# Write a function `remove_dupes(lst)` that accepts a list and returns a new list
+# where each element appears only once.
+
+
+def remove_dupes(list):
+    new_list = []
+    for el in list:
+        if el not in new_list:
+            new_list.append(el)
+    return new_list
+
+
+print(remove_dupes(["x", "y", "y", "x", "z"]))  # ['x', 'y', 'z']
+print(remove_dupes([False, False, True, False]))  # [False, True]
+print(remove_dupes([42, 5, 7, 42, 7, 3, 7, 7]))  # [42, 5, 7, 3]
+
