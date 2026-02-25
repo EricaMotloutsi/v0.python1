dddb40ef32a003623fb386661402f89f863c9022
Erica Motloutsi <ericamotloutsi@gmail.com>
2026-02-25 17:48:52 +0000

    Add function

---
+++ decomposition/a_decomposition/most_letter_word.py
@@ -0,0 +1,43 @@
+# Write a function `most_letter_word(sentence, char)` that accepts:
+# - a sentence string
+# - a single character
+#
+# The function should return the word in the sentence that contains the
+# character the greatest number of times.
+#
+# You can assume the sentence contains at least one word.
+# If there is a tie, return the word that appears first in the sentence.
+
+
+
+def most_letter_word(sentence, char):
+    dictionary = {}
+    list = sentence.split(" ")
+    for word in list:
+        char_count= word.count(char)
+        dictionary[word] = char_count
+    val = 0
+    key = ""
+    for k, v in dictionary.items():
+        if v >= val:
+            val = v
+            key = k 
+    return key 
+
+
+
+print(most_letter_word(
+'she received an award for excellence in science','e'
+))# 'excellence'
+
+print(most_letter_word(
+'she received an award for excellence in science','a'
+))# 'award'
+
+print(most_letter_word(
+'I hope sophomore year comes soon','o'
+))# 'sophomore'
+
+print(most_letter_word(
+'I hope sophomore year comes soon','s'
+))# 'sophomore'
