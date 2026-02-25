b7d9c737ced069529c768b15b89e22f663ef8d59
Erica Motloutsi <ericamotloutsi@gmail.com>
2026-02-25 09:01:13 +0000

    Combinations with two lists

---
+++ nesting/a_nesting/pair_print.py
@@ -0,0 +1,6 @@
+def pair_print(list):
+    for i in range(len(list)):
+        for j in range(i+1, len(list)):
+            print(list[i], list[j])
+
+pair_print(["artichoke", "broccoli", "carrot", "daikon"])
---
+++ nesting/a_nesting/print_combinations.py
@@ -0,0 +1,8 @@
+def print_combinations(colors, clothes):
+        for color in colors:
+            for cloth in clothes:
+                 print(color,cloth)
+
+colors = ["gray","cream", "cyan"]
+clothes = ["shirt", "flannel"]
+print_combinations(colors,clothes)
